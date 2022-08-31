689d95156be06e6e0fd0740d9480b3555f0d05eb
Jayson-Roda <roda.jayson.a@gmail.com>
2022-08-31 12:21:11 +0800

    client and backend

---
+++ backend/server.js
@@ -0,0 +1,34 @@
+if(process.env.NODE_ENV !== 'production'){
+    require('dotenv').config()
+}
+
+const express = require('express');
+const mongoose = require('mongoose');
+
+const workoutRoutes = require('./routes/workoutRoutes');
+
+// express app
+const app = express();
+
+// middleware
+app.use(express.json())
+
+app.use((req, res, next) => {
+    console.log(req.path, req.method)
+    next()
+})
+
+// routes
+app.use('/api/workouts', workoutRoutes)
+
+
+// connect to mongodb
+mongoose.connect(process.env.MONGODB_URL)
+    .then(() => {
+        app.listen(process.env.PORT, () => {
+            console.log(`Listening on port ${process.env.PORT}`)
+        })
+    })
+    .catch((err) => {
+        console.log(err)
+    })
